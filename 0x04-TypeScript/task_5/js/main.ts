d3db3335d816339b925adf592eeaf8fc39daebc8
benjamin-nyankson <117271631+benjamin-nyankson@users.noreply.github.com>
2026-01-29 20:57:29 +0000

    add subjects

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,12 +1,17 @@
-// Function to sum MajorCredits
-interface MajorCredits{
-    credits:number
+export interface MajorCredits {
+  credits: number;
 }
-export function sumMajorCredits(subject1: MajorCredits, subject2: {MajorCredits): number {
+
+export interface MinorCredits {
+  credits: number;
+}
+
+// Function to sum MajorCredits
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
   return subject1.credits + subject2.credits;
 }
 
 // Function to sum MinorCredits
-export function sumMinorCredits(subject1: { MinorCredits: number }, subject2: { MinorCredits: number }): number {
-  return subject1.MinorCredits + subject2.MinorCredits;
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
+  return subject1.credits + subject2.credits;
 }
